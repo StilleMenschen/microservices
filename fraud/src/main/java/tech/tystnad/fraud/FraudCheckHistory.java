2aa34c01eee4f7240be00a5d9c94d95a83212cbb
Stillemenschen <2956686701@qq.com>
2022-07-08 18:50:53 +0800

    feat: add fraud service

---
+++ fraud/src/main/java/tech/tystnad/fraud/FraudCheckHistory.java
@@ -0,0 +1,33 @@
+package tech.tystnad.fraud;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FraudCheckHistory {
+
+    @Id
+    @SequenceGenerator(
+            name = "fraud_check_history_id_sequence",
+            sequenceName = "fraud_check_history_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_check_history_id_sequence"
+    )
+    private Long id;
+    private Long customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
+}
---
+++ fraud/src/main/java/tech/tystnad/fraud/FraudCheckService.java
@@ -0,0 +1,24 @@
+package tech.tystnad.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class FraudCheckService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public boolean isFraudulentCustomer(Long customerId) {
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraudster(Boolean.FALSE)
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+        return Boolean.FALSE;
+    }
+}
