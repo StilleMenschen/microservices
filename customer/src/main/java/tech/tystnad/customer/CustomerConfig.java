11ed9d5f4a070556fd54287af615c8ea35d36902
Stillemenschen <2956686701@qq.com>
2022-07-09 11:07:36 +0800

    feat: service registration and discovery

--- customer/src/main/java/tech/tystnad/customer/CustomerConfig.java
+++ customer/src/main/java/tech/tystnad/customer/CustomerConfig.java
@@ -1,6 +1,7 @@
 package tech.tystnad.customer;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import java.time.Duration;
 public class CustomerConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplateBuilder()
                 .setConnectTimeout(Duration.ofMinutes(30))
